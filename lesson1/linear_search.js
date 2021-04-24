ccca57307f5efdc51df75730ad1273fc7d3f631b
MdTasib <ohidulalam442@gmail.com>
2021-04-24 12:25:35 +0600

    linear_search

---
+++ lesson1/linear_search.js
@@ -0,0 +1,8 @@
+const numbers = [10, 20, 30, 40, 50, 60, 70, 80];
+
+for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    if (element === 30) {
+        console.log('Index Number -- ', i);
+    }
+}
